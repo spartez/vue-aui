4531f60b7ffb0cebf914b5df9966a273bbbeb722
dskrodzki <damian.skrodzki@spartez.com>
2016-12-08 19:53:30 +0100

    Update dist

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -39,7 +39,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     new webpack.optimize.OccurrenceOrderPlugin(),
     // extract css into its own file
-    new ExtractTextPlugin(utils.assetsPath('css/[name].[contenthash].css')),
+    new ExtractTextPlugin(utils.assetsPath('css/[name].css')),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
