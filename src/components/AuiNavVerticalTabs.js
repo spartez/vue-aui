146721ddeda260d232ef1648264546eeb403c906
dskrodzki <damian.skrodzki@spartez.com>
2017-02-13 11:10:25 +0100

    Refactoring, get rid of re-rendering of components.

--- src/components/AuiNavVerticalTabs.js
+++ src/components/AuiNavVerticalTabs.js
@@ -1,40 +1,26 @@
 import AuiNavVertical from "./AuiNavVertical.vue"
-import AuiNavGroup from "./AuiNavGroup.vue"
-import AuiNavItem from "./AuiNavItem.vue"
 import AuiNavTab from "./AuiNavTab.vue"
 
 export default {
   render(createElement) {
-    const groupsAndHeaders = this.$slots.default
-      .filter(s => s.componentOptions)
-      .filter(s => s.componentOptions.tag === 'aui-nav-group' || s.componentOptions.tag === 'aui-nav-header');
-
-    const auiNavGroups = groupsAndHeaders.map(groupOrHeader => {
+    const auiNavGroups = this.groupsAndHeaders.map(groupOrHeader => {
       if (groupOrHeader.componentOptions.tag === 'aui-nav-header') {
         return groupOrHeader
       } else {
-        // For some reason attaching event after VNode was created from component doesn't work afterwards
         const children = groupOrHeader.componentOptions && groupOrHeader.componentOptions.children || [];
-        const auiNavItems = children
-          .filter(s => s.componentOptions)
-          .filter(s => s.componentOptions.tag === 'aui-nav-item')
-          .map(tab => createElement(AuiNavItem, {
-            props: {
-              name: tab.componentOptions.propsData.name,
-              selected: tab.componentOptions.propsData.name === this.selectedTab,
-            },
-            nativeOn: {
+        children
+          .filter(s => (s.componentOptions && s.componentOptions.tag === 'aui-nav-item'))
+          .forEach(tab => {
+            tab.componentOptions.propsData.selected = tab.componentOptions.propsData.name === this.selectedTab
+            tab.data.on = {
               click: () => this.selectedTab = tab.componentOptions.propsData.name
             }
-          }));
-        return createElement(AuiNavGroup, [auiNavItems])
+          });
+        return groupOrHeader
       }
     });
 
-    const selectedItem = groupsAndHeaders
-      .reduce((a, b) => b.componentOptions.children ? a.concat(b.componentOptions.children) : a, [])
-      .filter(item => item.componentOptions)
-      .filter(item => item.componentOptions.tag === 'aui-nav-item')
+    const selectedItem = this.items
       .filter(item => item.componentOptions.propsData.name === this.selectedTab)[0];
 
     const tabElement = createElement(AuiNavTab, selectedItem && selectedItem.componentOptions.children)
@@ -49,15 +35,23 @@ export default {
     }
   },
 
-  created() {
-    const groupsAndHeaders = this.$slots.default
-      .filter(s => s.componentOptions)
-      .filter(s => s.componentOptions.tag === 'aui-nav-group' || s.componentOptions.tag === 'aui-nav-header');
+  computed: {
+    groupsAndHeaders() {
+      return this.$slots.default
+        .filter(item => item.componentOptions)
+        .filter(item => item.componentOptions.tag === 'aui-nav-group' || item.componentOptions.tag === 'aui-nav-header')
+    },
 
-    const selectedItem = groupsAndHeaders
-      .reduce((a, b) => b.componentOptions.children ? a.concat(b.componentOptions.children) : a, [])
-      .filter(item => item.componentOptions)[0]
-    this.selectedTab = selectedItem.componentOptions.propsData.name;
+    items() {
+      return this.groupsAndHeaders
+        .reduce((items, group) => group.componentOptions.children ? items.concat(group.componentOptions.children) : items, [])
+        .filter(item => item.componentOptions);
+    }
+  },
+
+  created() {
+    const firstItem = this.items[0]
+    this.selectedTab = firstItem.componentOptions.propsData.name;
   },
 
   methods: {
