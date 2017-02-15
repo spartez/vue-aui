a2ac19a20be51632dccf3da9d9c139cf6fcbf788
dskrodzki <damian.skrodzki@spartez.com>
2017-02-15 22:48:36 +0100

    Split buttons examples to 3 sections

--- src/docs/component/Buttons.vue
+++ src/docs/component/Buttons.vue
@@ -1,22 +1,47 @@
 <template>
   <div>
-    <h3>Toggle button</h3>
+    <h3>Button types</h3>
     <aui-tabs>
       <aui-tab name="Example">
-        <h3>Button types</h3>
         <p>
           <aui-button>Button</aui-button>
+        </p>
+        <p>
           <aui-button type="primary">Primary button</aui-button>
+        </p>
+        <p>
           <aui-button type="link">Button link</aui-button>
         </p>
-        <h3>Button variations</h3>
+      </aui-tab>
+      <aui-tab name="Code">
+        <pre v-highlightjs><code class="xml" v-text='code'></code></pre>
+      </aui-tab>
+    </aui-tabs>
+    <h3>Button variations</h3>
+
+    <aui-tabs>
+      <aui-tab name="Example">
         <p>
           <aui-button disabled>Button</aui-button>
+        </p>
+        <p>
           <aui-button light>Light</aui-button>
+        </p>
+        <p>
           <aui-button subtle>Subtle</aui-button>
+        </p>
+        <p>
           <aui-button compact>Compact</aui-button>
         </p>
-        <h3>Button as link</h3>
+      </aui-tab>
+      <aui-tab name="Code">
+        <pre v-highlightjs><code class="xml" v-text='code2'></code></pre>
+      </aui-tab>
+    </aui-tabs>
+    <h3>Button as link</h3>
+
+    <aui-tabs>
+      <aui-tab name="Example">
         <p>
           Just add href to the button.
         </p>
@@ -26,7 +51,7 @@
         </p>
       </aui-tab>
       <aui-tab name="Code">
-        <pre v-highlightjs><code class="xml" v-text='code'></code></pre>
+        <pre v-highlightjs><code class="xml" v-text='code3'></code></pre>
       </aui-tab>
     </aui-tabs>
 
@@ -37,27 +62,15 @@
   export default {
     data() {
       return {
-        code: `<h3>Button types</h3>
-<p>
-  <aui-button>Button</aui-button>
-  <aui-button type="primary">Primary button</aui-button>
-  <aui-button type="link">Button link</aui-button>
-</p>
-<h3>Button variations</h3>
-<p>
-  <aui-button disabled>Button</aui-button>
-  <aui-button light>Light</aui-button>
-  <aui-button subtle>Subtle</aui-button>
-  <aui-button compact>Compact</aui-button>
-</p>
-<h3>Button as link</h3>
-<p>
-  Just add href to the button.
-</p>
-<p>
-  <aui-button type="link" href="#">Link 1</aui-button>
-  <aui-button type="link" href="#">Link 2</aui-button>
-</p>`
+        code: `<aui-button>Button</aui-button>
+<aui-button type="primary">Primary button</aui-button>
+<aui-button type="link">Button link</aui-button>`,
+        code2: `<aui-button disabled>Button</aui-button>
+<aui-button light>Light</aui-button>
+<aui-button subtle>Subtle</aui-button>
+<aui-button compact>Compact</aui-button>`,
+        code3: `<aui-button type="link" href="#">Link 1</aui-button>
+<aui-button type="link" href="#">Link 2</aui-button>`,
       }
     }
   }
