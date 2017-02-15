a41c21e06fe4807d454b164bdbf89563375afa60
dskrodzki <damian.skrodzki@spartez.com>
2017-02-15 23:07:39 +0100

    Reorder component registration alphabetically

--- src/components/VueAui.js
+++ src/components/VueAui.js
@@ -1,37 +1,40 @@
 import AuiButton from './buttons/AuiButton'
-
 import AuiToggleButton from './buttons/AuiToggleButton.vue'
-import AuiSelect2Single from './select2/AuiSelect2Single.vue'
+
+import AuiNavGroup from './navigation/AuiNavGroup.vue'
+import AuiNavHeader from './navigation/AuiNavHeader.vue'
+import AuiNavItem from './navigation/AuiNavItem.vue'
+import AuiNavTab from './navigation/AuiNavTab.vue'
+import AuiNavVerticalRouter from './navigation/AuiNavVerticalRouter.js'
+
 import AuiSelect2Multi from './select2/AuiSelect2Multi.vue'
 import AuiSelect2Option from './select2/AuiSelect2Option.vue'
+import AuiSelect2Single from './select2/AuiSelect2Single.vue'
 
 import AuiDialog from './AuiDialog.vue'
 import AuiLozenge from './AuiLozenge.vue'
 
-import AuiTabs from './tabs/AuiTabs.vue'
 import AuiTab from './tabs/AuiTab.vue'
-
-import AuiNavItem from './navigation/AuiNavItem.vue'
-import AuiNavHeader from './navigation/AuiNavHeader.vue'
-import AuiNavGroup from './navigation/AuiNavGroup.vue'
-import AuiNavTab from './navigation/AuiNavTab.vue'
-import AuiNavVerticalRouter from './navigation/AuiNavVerticalRouter.js'
+import AuiTabs from './tabs/AuiTabs.vue'
 
 export default {
   install(Vue, options) {
     Vue.component('aui-button', AuiButton)
-    Vue.component('aui-select2-single', AuiSelect2Single)
-    Vue.component('aui-select2-multi', AuiSelect2Multi)
-    Vue.component('aui-select2-option', AuiSelect2Option)
     Vue.component('aui-toggle-button', AuiToggleButton)
-    Vue.component('aui-dialog', AuiDialog)
-    Vue.component('aui-lozenge', AuiLozenge)
-    Vue.component('aui-tabs', AuiTabs)
-    Vue.component('aui-tab', AuiTab)
-    Vue.component('aui-nav-item', AuiNavItem)
-    Vue.component('aui-nav-header', AuiNavHeader)
+
     Vue.component('aui-nav-group', AuiNavGroup)
+    Vue.component('aui-nav-header', AuiNavHeader)
+    Vue.component('aui-nav-item', AuiNavItem)
     Vue.component('aui-nav-tab', AuiNavTab)
     Vue.component('aui-nav-vertical', AuiNavVerticalRouter)
+
+    Vue.component('aui-select2-multi', AuiSelect2Multi)
+    Vue.component('aui-select2-option', AuiSelect2Option)
+    Vue.component('aui-tabs', AuiTabs)
+    Vue.component('aui-tab', AuiTab)
+    Vue.component('aui-select2-single', AuiSelect2Single)
+
+    Vue.component('aui-dialog', AuiDialog)
+    Vue.component('aui-lozenge', AuiLozenge)
   }
 }
