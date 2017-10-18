24c454ac1b7309d14b5a56b260dd8f30e0134b2a
dskrodzki <damian.skrodzki@spartez.com>
2017-10-18 18:22:40 +0200

    Add build script check for uncommitted changes and missing npm version bump.

--- build/publish.sh
+++ build/publish.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+  echo "Please specify the version. Use one of patch, minor or major."
+  exit 1
+fi
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "There are uncommitted changes. Clean them up first";
+  exit 1
+fi
+
 NEW_VERSION=$(npm version $1)
 npm run build
-git commit -am "Updating dist to version $NEW_VERSION"
+git commit -am "Updating dist to version $NEW_VERSION" -- ./dist
 git push origin master --tags
 npm publish
