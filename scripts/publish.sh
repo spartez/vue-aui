7e9979195cf410401094c536c517a3561606c723
dskrodzki <damian.skrodzki@spartez.com>
2017-12-19 12:41:22 +0100

    Fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -12,6 +12,6 @@ fi
 
 NEW_VERSION=$(npm version $1)
 npm run build
-git commit -am "Updating dist to version $NEW_VERSION" -- ./dist ./index.html
+git commit -m "Updating dist to version $NEW_VERSION" -- ./dist ./index.html
 git push origin master --tags
 npm publish
