2ec5c53c098568b914e92dd766290b7493573501
dskrodzki <damian.skrodzki@spartez.com>
2018-01-18 16:02:41 +0100

    Remove obsolete CSS fixes for AUI 7

--- src/components/select2/AuiSelect2Single.vue
+++ src/components/select2/AuiSelect2Single.vue
@@ -36,9 +36,4 @@
   .aui-select2-container.select2-container.select2-allowclear .select2-choice abbr {
     display: block;
   }
-
-  [data-aui-version^="7"] .aui-select2-container.select2-container.select2-allowclear .select2-choice abbr {
-    padding-top: 0;
-    top: 8px;
-  }
 </style>
