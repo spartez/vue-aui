ba6af86768011c084101f34ce027f96b380c4283
Damian Skrodzki <damian.skrodzki@spartez.com>
2018-10-09 12:40:37 +0200

    Update docs about deprecates progress bar

--- src/docs/component/ProgressIndicatorDocs.vue
+++ src/docs/component/ProgressIndicatorDocs.vue
@@ -1,26 +1,22 @@
 <template>
   <div class="aui-group">
+
     <div class="aui-item">
+      <va-message>
+        AUI 7.7 introduced progress bar web component {{`&lt;aui-progressbar&gt;`}} which functionally replaces {{`&lt;va-progress-indicator&gt;`}}.
+        You can still use {{`&lt;va-progress-indicator&gt;`}} on AUI older than 7.7. See
+        <a href="https://docs.atlassian.com/aui/latest/docs/progress-indicator.html" target="_blank">
+          aui-progress web component documentation</a>.
+      </va-message>
       <va-tabs>
-        <va-tab name="Example">
-          <p>
-            <va-progress-indicator :progress='0.3'></va-progress-indicator>
-          </p>
-          <p>
-            <va-progress-indicator indeterminate></va-progress-indicator>
-          </p>
-          <p>
-            <va-progress-indicator :indeterminate='indeterminate' :progress="0.5"></va-progress-indicator>
-          </p>
-        </va-tab>
-        <va-tab name="Code">
+        <va-tab name="Deprecated code">
           <pre v-highlightjs><code class="xml" v-text='code'></code></pre>
         </va-tab>
       </va-tabs>
     </div>
 
     <div class="aui-item">
-      <api-table name="va-progress-indicator" :props="[
+      <api-table name="va-progress-indicator" is-deprecated :props="[
       {name: 'progress', type: 'Number', default: '0', description: 'Sets progress bar progress in scale 0-100.'},
       {name: 'indeterminate', type: 'Boolean', default: 'false', description: 'Renders infinite progress bar.'},
     ]">
