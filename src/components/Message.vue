a8f1e1c659a924c4bacfa4c40d068b41c15f945e
Igor Guastalla de Lima <limaguastallaigor@gmail.com>
2018-10-15 21:55:48 -0300

    Added transition props for message component. Available values: fade-in, fade-in-top, fade-in-bottom, fade-out, fade-out-top, fade-out-bottom. Updated docs and added examples.

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="classObject">
+  <div :class="[classObject, transition]">
     <p v-if="title" class="title">
       <strong>{{title}}</strong>
     </p>
@@ -22,6 +22,13 @@
             || 'info' === value
         }
       },
+      transition: {
+        type: String,
+        validator(value) {
+          const transitionArray = ['fade-in-bottom', 'fade-in-top', 'fade-in', 'fade-out-top', 'fade-out-bottom', 'fade-out'];
+          return transitionArray.includes(value);
+        },
+      },
     },
 
     computed: {
@@ -30,12 +37,27 @@
           'aui-message': true,
           [`aui-message-${this.type}`]: true,
           'closeable': this.closeable,
-        }
-      }
+        };
+      },
     },
 
     mounted() {
       AJS.messages.makeCloseable();
-    }
-  }
+    },
+  };
 </script>
+
+<style scoped>
+.fade-in-bottom{-webkit-animation:fade-in-bottom .6s cubic-bezier(.39,.575,.565,1.000) both;animation:fade-in-bottom .6s cubic-bezier(.39,.575,.565,1.000) both}
+@-webkit-keyframes fade-in-bottom{0%{-webkit-transform:translateY(50px);transform:translateY(50px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}@keyframes fade-in-bottom{0%{-webkit-transform:translateY(50px);transform:translateY(50px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}
+.fade-in-top{-webkit-animation:fade-in-top .6s cubic-bezier(.39,.575,.565,1.000) both;animation:fade-in-top .6s cubic-bezier(.39,.575,.565,1.000) both}
+@-webkit-keyframes fade-in-top{0%{-webkit-transform:translateY(-50px);transform:translateY(-50px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}@keyframes fade-in-top{0%{-webkit-transform:translateY(-50px);transform:translateY(-50px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}
+.fade-in{-webkit-animation:fade-in 1.2s cubic-bezier(.39,.575,.565,1.000) both;animation:fade-in 1.2s cubic-bezier(.39,.575,.565,1.000) both}
+@-webkit-keyframes fade-in{0%{opacity:0}100%{opacity:1}}@keyframes fade-in{0%{opacity:0}100%{opacity:1}}
+.fade-out{-webkit-animation:fade-out 1s ease-out both;animation:fade-out 1s ease-out both}
+@-webkit-keyframes fade-out{0%{opacity:1}100%{opacity:0}}@keyframes fade-out{0%{opacity:1}100%{opacity:0}}
+.fade-out-top{-webkit-animation:fade-out-top .7s cubic-bezier(.25,.46,.45,.94) both;animation:fade-out-top .7s cubic-bezier(.25,.46,.45,.94) both}
+@-webkit-keyframes fade-out-top{0%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}100%{-webkit-transform:translateY(-50px);transform:translateY(-50px);opacity:0}}@keyframes fade-out-top{0%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}100%{-webkit-transform:translateY(-50px);transform:translateY(-50px);opacity:0}}
+.fade-out-bottom{-webkit-animation:fade-out-bottom .7s cubic-bezier(.25,.46,.45,.94) both;animation:fade-out-bottom .7s cubic-bezier(.25,.46,.45,.94) both}
+@-webkit-keyframes fade-out-bottom{0%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}100%{-webkit-transform:translateY(50px);transform:translateY(50px);opacity:0}}@keyframes fade-out-bottom{0%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}100%{-webkit-transform:translateY(50px);transform:translateY(50px);opacity:0}}
+</style>
--- src/docs/component/MessagesDocs.vue
+++ src/docs/component/MessagesDocs.vue
@@ -42,14 +42,55 @@
           <pre v-highlightjs><code class="xml" v-text='code2'></code></pre>
         </va-tab>
       </va-tabs>
+
+      <h3>Transitions</h3>
+
+      <va-tabs>
+        <va-tab name="Example">
+          <va-message class="go-flex" transition="fade-in" v-show="fadeIn">
+            <span>Fade in</span>
+            <span class="spacer"></span>
+            <va-button @click="toggle('fadeIn')" type="primary">Toggle</va-button>
+          </va-message>
+          <va-message class="go-flex" transition="fade-in-top" v-show="fadeInTop">
+            <span>Fade in top</span>
+            <span class="spacer"></span>
+            <va-button @click="toggle('fadeInTop')" type="primary">Toggle</va-button>
+          </va-message>
+          <va-message class="go-flex" transition="fade-in-bottom" v-show="fadeInBottom">
+            <span>Fade in bottom</span>
+            <span class="spacer"></span>
+            <va-button @click="toggle('fadeInBottom')" type="primary">Toggle</va-button>
+          </va-message>
+          <va-message class="go-flex" :transition="fadeOut ? 'fade-out' : ''">
+            <span>Fade out</span>
+            <span class="spacer"></span>
+            <va-button @click="toggle('fadeOut')" type="primary">Toggle</va-button>
+          </va-message>
+          <va-message class="go-flex" :transition="fadeOutTop ? 'fade-out-top' : ''">
+            <span>Fade out top</span>
+            <span class="spacer"></span>
+            <va-button @click="toggle('fadeOutTop')" type="primary">Toggle</va-button>
+          </va-message>
+          <va-message class="go-flex" :transition="fadeOutBottom ? 'fade-out-bottom' : ''">
+            <span>Fade out bottom</span>
+            <span class="spacer"></span>
+            <va-button @click="toggle('fadeOutBottom')" type="primary">Toggle</va-button>
+          </va-message>
+        </va-tab>
+        <va-tab name="Code">
+          <pre v-highlightjs><code class="xml" v-text='code3'></code></pre>
+        </va-tab>
+      </va-tabs>
     </div>
 
     <div class="aui-item">
       <api-table name="va-message" :props="[
       {name: 'title', type: 'String', description: 'Bold title of the message.'},
       {name: 'type', type: 'String', default: 'info', description: 'Colored type of a message. Values: error, warning, success, info.'},
+      {name: 'transition', type: 'String', description: 'Transitions of the message. Available values are: fade-in, fade-in-top, fade-in-bottom, fade-out, fade-out-top, fade-out-bottom.'},
     ]" :slots="[
-      {name: 'default', description: 'Renders dialog contents.'},
+      {name: 'default', description: 'Renders dialog contents.'}
     ]">
       </api-table>
     </div>
@@ -79,7 +120,53 @@
     <li><button class="aui-button aui-button-link">Take your chances</button></li>
   </ul>
 </va-message>`,
+        code3: `<va-message transition="fade-in">
+    <span>Fade in</span>
+</va-message>
+
+<va-message transition="fade-in-top">
+    <span>Fade in top</span>
+</va-message>
+
+<va-message transition="fade-in-bottom">
+    <span>Fade in bottom</span>
+</va-message>
+
+<va-message transition="fade-out">
+    <span>Fade out</span>
+</va-message>
+
+<va-message transition="fade-out-top">
+    <span>Fade out top</span>
+</va-message>
+
+<va-message transition="fade-out-bottom">
+    <span>Fade out bottom</span>
+</va-message>`,
+        fadeIn: true,
+        fadeInTop: true,
+        fadeInBottom: true,
+        fadeOut: false,
+        fadeOutTop: false,
+        fadeOutBottom: false
+      }
+    },
+    methods: {
+      toggle(fadeName) {
+        this[fadeName] = !this[fadeName];
+        setTimeout(() => { this[fadeName] = !this[fadeName]; }, 800);
       }
     }
   }
 </script>
+
+<style scoped>
+.go-flex {
+  display: flex;
+  flex-flow: row nowrap;
+}
+
+.spacer {
+  flex-grow: 1;
+}
+</style>
